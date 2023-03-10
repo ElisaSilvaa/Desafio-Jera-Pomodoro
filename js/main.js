48ddc971de263ab8c8e963cb76c83eab9ceb5b88
ElisaSilvaa <elisabethy68@gmail.com>
2023-03-10 18:39:14 -0300

    Git commit - "Add const e audio no js"

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const circularProgressbar = document.querySelector("#circularProgressBar");
+const circularProgressBarNumber = document.querySelector("#circularProgressBar .progress-value");
+const buttonTypePomodoro = document.querySelector("#buttonTypePomodoro");
+const buttonTypeShortBreak = document.querySelector("#buttonTypeShortBreak");
+
+const audio = new Audio('BIP.mp3');
