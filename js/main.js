7034b6a8e40a7d2355189dd9fd21d746b55a6c6f
ElisaSilvaa <elisabethy68@gmail.com>
2023-03-10 19:03:19 -0300

    git commit - 'start timer'

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
-const circularProgressbar = document.querySelector("#circularProgressBar");
-const circularProgressBarNumber = document.querySelector("#circularProgressBar .progress-value");
-const buttonTypePomodoro = document.querySelector("#buttonTypePomodoro");
-const buttonTypeShortBreak = document.querySelector("#buttonTypeShortBreak");
+    const circularProgressbar = document.querySelector("#circularProgressBar");
+    const circularProgressBarNumber = document.querySelector("#circularProgressBar .progress-value");
+    const buttonTypePomodoro = document.querySelector("#buttonTypePomodoro");
+    const buttonTypeShortBreak = document.querySelector("#buttonTypeShortBreak");
 
-const audio = new Audio('BIP.mp3');
+    const audio = new Audio('BIP.mp3');
+
+    const pomodorotimerInSeconds = 1500;
+    const shortBreakTimerInSeconds = 100;
+    const TIMER_TYPE_POMODORO = 'POMODORO';
+    const TIMER_TYPE_SHORT_BREAK = 'SHORTBREAK';
+
+    let progressInterval;
+    let pomodoroType = TIMER_TYPE_POMODORO;
+    let timerValue = pomodorotimerInSeconds;
+    let multiplierFactor = 360 / timerValue;
+
+
+    function formatNumberInStringMinute(number) {
+        const minutes = Math.trunc(number / 60)
+            .toString()
+            .padStart(2, '0');
+        const seconds = Math.trunc(number % 60)
+            .toString()
+            .padStart(2, '0');
+        return `${minutes}:${seconds}`;
+    }
+
+    const startTimer = () => {
+        progressInterval = setInterval(() => {
+            timerValue--;
+            setInfoCircularProgressBar();
+        }, 1000);
+    }
